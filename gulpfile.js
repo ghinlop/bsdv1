362342cdf2b622322a72abfd58ad3189559ad752
Ghin Lop <ghinlop@gmail.com>
2018-05-03 08:34:02 +0700

    Fixed

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,9 @@ const gulp          = require('gulp')
     , st            = require('st')
     , path          = require('path')
     , plumber       = require('gulp-plumber')
-    
-gulp.task('copy', () => {
-    gulp.src([
-        'dev/dist/**/*.*',
-        '!dev/dist/**/*.scss'
-
-    ])
-        .pipe(gulp.dest('./prod/dist'))
-})
 
 gulp.task('sass', () => {
-   gulp.src('./dev/dist/main/scss/*.scss')
+   gulp.src('./dev/scss/*.scss')
         .pipe(plumber({
             errorHandler: function (error) {
                     console.log(error.toString());
@@ -28,7 +19,7 @@ gulp.task('sass', () => {
         )
         .pipe(sass())
         .pipe(minify())
-        .pipe(gulp.dest('./prod/dist/main/css'))
+        .pipe(gulp.dest('/dist/main/css'))
         .pipe(liveReload())
 });
 
@@ -42,13 +33,13 @@ gulp.task('pug', () => {
         })
     )
     .pipe(pug({pretty: true}))
-    .pipe(gulp.dest('./prod'));
+    .pipe(gulp.dest( path.join(__dirname, '/')));
 })
 
 gulp.task('watch-sass', () => {
     liveReload.listen();
     gulp.watch([
-        './dev/dist/main/scss/**/*.scss'],
+        './dev/scss/**/*.scss'],
         ['sass']);
 })
 gulp.task('watch-pug', () => {
@@ -60,8 +51,8 @@ gulp.task('watch-pug', () => {
 
 gulp.task('server', function(done) {
     http.createServer(
-      st({ path: __dirname + '/prod', index: 'index.html', cache: false })
+      st({ path: __dirname + '/', index: 'index.html', cache: false })
     ).listen(8080, done);
-    gulp.run(['watch-sass', 'watch-pug', 'copy'])
+    gulp.run(['watch-sass', 'watch-pug'])
 });
 
